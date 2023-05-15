070f7ee030d9dfe037973766e42c369735c610c3
Gabrieli <102187337+vitoria-ms@users.noreply.github.com>
2023-05-15 13:24:00 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./img/incon.png">
+    <link href="style.css" rel="stylesheet">
+    <title>Sua caixa de ferramentas na nuvem.
+     Escolheu, usou. | EasyTools</title>
+     <meta name="description" content="EasyTools, sua caixa de 
+     ferramenta na nuvem! Precisou de ferramentas
+     e equipamentos? Peça na EasyTools." data-react-helmet="true">
+</head>
+<body>
+    <!--Cabeçalho da página/ Links, logo, área de Pesquisa-->
+    <nav class="cabecalho">
+        <div class="cabecalho-logo">
+            <div class="cabecalho-logo-imagem">
+                <a href="index.html"><img class="cabecalho-imagem" src="./img/incon.png" alt="Logo MARCA SITE LOJA"></a>
+            </div>
+        </div>
+        <div class="search-box">
+            <input type="text" class="search-txt" placeholder=" Qual ferramenta para hoje?">
+     <div class="search-btn" href="#">
+    <!--img-->
+
+            </div>
+        </div>
+        <div class="buttons">
+    
+        <a href="">Ganhe dinheiro!</a>
+        <p class="btn-nav"><img>Entrar</p>
+        <a href=""><img>Dúvidas</a>
+        <div><p class="btn-nav-menu"><img>Menu</p>
+    <div id="menu-side">
+    <a href="/s/about-us" target="_blanck" class="navbar-item link-how-work">Como funciona?</a>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSc73i4iPSCEIlLe5BD83eL1ZL89AoBCdZgcr4tCd8iJaH2nzQ/viewform" rel="noreferrer" class="navbar-item neighboor-nav" target="_blank">Ganhe dinheiro investindo em ferramentas!</a>
+    <a href="https://caixadeferramenta.easytoolsapp.com/" target="_blanck" class="navbar-item link-how-work">Blog da easy</a>
+    <a href="href=&quot;https://api.whatsapp.com/send?phone=5541991695587&amp;text=Ol%C3%A1,%20tenho%20d%C3%BAvidas.&quot;" target="_blank" class="navbar-item link-how-work">Dúvidas - Whatsapp</a>
+    </div></div>
+    </div>
+</nav>
+</body>
+</html>
