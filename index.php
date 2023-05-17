e523e07abb5d25b978eb2d594959dead1b6e952f
Lauanda Nobre <113943598+lauandanobre@users.noreply.github.com>
2023-05-16 21:53:19 -0300

    Aprimorando css e html

--- index.php
+++ index.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="estilo/style.css" rel="stylesheet">
     <title>ToolsApp</title>
 </head>
 <body>
-    
+    <header>
+
+    </header>
+    <main class="principal-main">
+        <!-- Esse é o main principal -->
+        <div class="box1">
+            <h1>Aluguel de ferramentas</h1>
+            <div class="box-btn">
+                <button class="btn-estilo1">Alugue agora</button>
+                <button class="btn-estilo2">Nos contate</button>
+            </div>
+        </div>
+        <div class="box2">
+            <img src="imagens/painel.png" id="painel" alt="iamgem de pessoas trabalhando">
+        </div>
+    </main>
 </body>
 </html>
