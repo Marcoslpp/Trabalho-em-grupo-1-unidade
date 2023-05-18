89688aabac694620d1bf1eebe6401b6f135741da
Marcoslpp <132162191+Marcoslpp@users.noreply.github.com>
2023-05-18 08:46:31 -0300

    index.php
    Adição das linhas 29 até a 88

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="estilo/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
     <title>ToolsApp</title>
 </head>
+
 <body>
     <header>
 
@@ -14,7 +16,7 @@
     <main class="principal-main">
         <!-- Esse é o main principal -->
         <div class="box1">
-            <h1>Aluguel de ferramentas</h1>
+            <h1>Aluguel de <br>ferramentas</h1>
             <div class="box-btn">
                 <button class="btn-estilo1">Alugue agora</button>
                 <button class="btn-estilo2">Nos contate</button>
@@ -23,7 +25,66 @@
         <div class="box2">
             <img src="imagens/painel.png" id="painel" alt="iamgem de pessoas trabalhando">
         </div>
-        <p>Apenas testando</p>
     </main>
+
+
+    <section id="section1">
+        <div id="cabSection1">
+            <h2>O que você precisa?</h2>
+        </div>
+
+        <div class="container">
+            <div class="card">
+                <img src="imagens/jardinagem.jpg" alt="Imagem de jardinagem" class="img-section">
+                <span>Jardinagem</span>
+            </div>
+            <div class="card">
+                <img src="imagens/construcao.jpg" alt="Imagem de construcao" class="img-section">
+                <span>construção</span>
+            </div>
+            <div class="card">
+                <img src="imagens/limpeza.jpg" alt="Imagem de limpeza" class="img-section">
+                <span>Limpeza</span>
+            </div>
+            <div class="card">
+                <img src="imagens/ferramentas2.jpg" alt="Imagem com ferramentas" class="img-section">
+                <span>Faça você mesmo</span>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-secundario">
+            <button class="btn3"><a href="">Ver todas as ferramentas e equipamentos</a></button>
+    </section>
+
+    <h3>Como funciona a Tools?</h3>
+    <section class="section-terciario">
+        
+        <div class="div-section-1">
+            <img src="imagens/ferramentas.png" class="i" alt="Ferramentas">
+            <span>1.Escolha a ferramenta</span>
+        </div>
+        <div class="div-section-2">
+            <img src="imagens/relogio.png" class="i" alt="Relógio">
+            <span>2.Selecioneo período de uso</span>
+        </div>
+        <div class="div-section-3">
+            <img src="imagens/lar-doce-lar.png" class="i" alt="Casa com um coração na lateral">
+            <span>3.receba em seu lar</span>
+        </div>
+    </section>
+
+    <section class="section-quaternario">
+        <button class="btn4"><a href="">Alugue agora</a></button>
+    </section>
+
+    <footer>
+        <h3>Entre em contato</h3>
+        <img src="" alt="Logo do site">
+
+        <div id="endereco">
+            <p></p>
+        </div>
+    </footer>
 </body>
 </html>
