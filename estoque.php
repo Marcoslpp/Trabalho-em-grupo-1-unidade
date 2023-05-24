866ec44d7f1fa912f040d028286561e7b48334f7
joshua <202115210035@ifba.edu.br>
2023-05-24 17:26:30 -0300

    espaço

--- estoque.php
+++ estoque.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="estilo/style.css" rel="stylesheet">
     <title>ToolsApp</title>
-</head>
+</head> 
 <body>
     <header>
 
